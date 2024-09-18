0c40a87b784020e5a43f8d2cdf5c4a2d5e151366
bishal-debug <bishalbist2003@gmail.com>
2024-09-18 12:26:59 -0400

    Update Week2.java

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -13,8 +13,10 @@ public class Week2 {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        // hello all this is pull
         System.out.println("Hello World");
     }
     
